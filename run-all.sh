feb076a05427c72547f2d6c465e5e8d8af70c637
= <xysiek99@gmail.com>
2022-12-17 15:51:34 +0100

    Fixed automation script - added terraform init

--- run-all.sh
+++ run-all.sh
@@ -6,13 +6,16 @@
 echo "1. Create key pair for ssh"
 ssh-keygen -b 4096 -t rsa -f ~/.ssh/test_vm_key -q -N ""
 
-echo "2. Setting up infrastructure with TF"
+echo "2. Init terraform"
 cd TF-setup
+terraform init
+
+echo "3. Setting up infrastructure with TF"
 terraform apply -auto-approve
 
-echo "3. Installing Docker on VM with Ansible"
+echo "4. Installing Docker on VM with Ansible"
 cd ../Ansible-setup
 ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook main.yml -i hosts
 
-echo "4. Creating CA authority and self-signed cert with Ansible"
+echo "5. Creating CA authority and self-signed cert with Ansible"
 ansible-playbook createCAandSSLcert.yml -i hosts
